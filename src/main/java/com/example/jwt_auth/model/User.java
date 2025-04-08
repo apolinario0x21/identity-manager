44c326c642ba72c5a97a3fe3cc11d2348749a04d
apolinario0x21 <imarcelo.apolinario@gmail.com>
2025-04-08 01:37:47 -0300

    autenticação e autorização com spring security

---
+++ src/main/java/com/example/jwt_auth/model/User.java
@@ -0,0 +1,41 @@
+package com.example.jwt_auth.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    @Column(unique = true, name = "email", nullable = false)
+    private String email;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
